25480825d4d50aeaa822155dd6fc8a90b3ba6d47
JJAJJAKIM <csm9121@gmail.com>
2024-05-16 15:02:32 +0900

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+		
+		int 숫자 = 1;
+		System.out.println(숫자 + "");
+		
+		int 숫자2;
+		//System.out.println(숫자2 + "");
+		
+		숫자2 = 숫자;
+		System.out.println(숫자2 + "");
+	}
+
+}
