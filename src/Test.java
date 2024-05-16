bac82719201aa126927cacc3cdccd765c40ad19a
JJAJJAKIM <csm9121@gmail.com>
2024-05-16 16:09:19 +0900

    Test.java

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!");
+/* 		System.out.println("Hello World !!");
 		
 		int 숫자 = 1;
 		System.out.println(숫자 + "");
@@ -12,6 +12,43 @@ public class Test {
 		
 		숫자2 = 숫자;
 		System.out.println(숫자2 + "");
+
+		final int qWer = 100;
+	    // qWer = 1;
+		
+		int 몸무게 = 80;
+		float 실수 = 몸무게;
+		
+		몸무게 = (int)실수;
+	    
+		int 변수 = 0;
+		System.out.println(변수); //0
+		int a = 변수++;
+		System.out.println(변수);
+		System.out.println(a);
+		
+		int b = 0;
+		
+		int c = a + b;
+		
+		int d = 5;
+		d += c;
+		System.out.println(d);
+*/		
+		int no = 10; // 초기값
+		no = no + 2; // 12
+		System.out.println(no);
+		no += 2; // 14
+		System.out.println(no);
+		no -= 2; // 12
+		System.out.println(no);
+		no *= 2; // 24
+		System.out.println(no);
+		no /= 2; // 12
+		System.out.println(no);
+		no %= 2; //0
+		System.out.println(no);
+		
 	}
 
 }
