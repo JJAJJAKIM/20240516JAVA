f4e6afa4cdc99088a4833fb3783a3857c5551a42
JJAJJAKIM <csm9121@gmail.com>
2024-05-17 17:03:04 +0900

    Add files via upload

---
+++ src/java0517/Test2.java
@@ -0,0 +1,92 @@
+package java0517;
+
+
+/*********************************
+ * >> 조건문 
+ * if 		: if(조건식) {}
+ * else	if	: if(조건식) {} else if(조건식) {}
+ * else		: if(조건식) {} else {}
+ *********************************/
+
+public class Test2{
+	
+	public static void main(String[] args) {
+		
+
+		/*
+		 * if(true) { System.out.println("참"); 논리값 = false; }
+		 * 
+		 * if(논리값) { System.out.println("논리값이 참이다."); }
+		 
+		
+		if(!논리값) {
+			System.out.println("부정의 논리값");
+		}
+		
+		int a, b;
+		a = 5;   // 
+		b = ++a; // 6
+		
+		if(a == b) {
+			System.out.println("a와 b는 같다.");
+			System.out.println(a);
+		} else {
+			System.out.println("a와 b는 다르다.");
+		*/
+		/*
+		 * boolean 논리값 = true;
+		 * 
+		 * if(!논리값) { 논리값 = !논리값; }else if (논리값) { System.out.println("참이다."); }
+		 */
+		
+		/*
+		 * int a, b; a = 10; b = 14;
+		 * 
+		 * if(a > b) { System.out.println("b가 크다."); } else if (a < b) {
+		 * System.out.println("a가 크다"); }
+		 */
+		/*
+		 * int a,b ; a = 5; b = 3;
+		 * 
+		 * if (a>b) { System.out.println(a + ">"+ b + "은 참이다."); } if (a>=b) {
+		 * System.out.println(a + ">="+ b + "은 참이다."); } if (!(a<b)) {
+		 * System.out.println(a + "<"+ b+ "은 거짓이다."); } if (!(a<=b)) {
+		 * System.out.println(a + "<="+ b + "은 거짓이다."); } if (a==a) {
+		 * System.out.println(a +"=="+ a +"은 참이다."); } if (a!=b) { System.out.println(a
+		 * + "!=" + b + "은 참이다."); }
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
